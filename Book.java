c46ca605cf99653fba6504227ae19f08d00517ba
RAY-ABEL <rayabel1251@gmail.com>
2019-05-14 15:02:27 +0100

    my first book program on git

---
+++ Book.java
@@ -0,0 +1,12 @@
+  public class Book{
+
+    private int pages;
+    private String author;
+    private String genre;
+    private boolean hardcover;
+
+
+    public static void main(String[] args){
+
+   }
+ }

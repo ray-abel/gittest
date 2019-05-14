85d13308d67cf183b78add86f0829819289983c1
RAY-ABEL <rayabel1251@gmail.com>
2019-05-14 16:08:43 +0100

    methods has been added

--- Book.java
+++ Book.java
@@ -5,7 +5,16 @@
     private String genre;
     private boolean hardcover;
 
-
+   Public void setPages(String pages){
+       this pages = pages;
+}  Public String getPages(){
+       return pages;
+}
+   Public void setAuthor(String author){
+       this author = author;
+}  Public String getAuthor(){
+       return author;
+}
     public static void main(String[] args){
 
    }
